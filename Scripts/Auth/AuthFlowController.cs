73333f0526e853d8d2598c5e4542e832d6bfffc1
MaxWu <WuMax13@gmail.com>
2026-03-28 23:17:17 +0800

    chore: format the authentication code

--- Scripts/Auth/AuthFlowController.cs
+++ Scripts/Auth/AuthFlowController.cs
@@ -1,5 +1,6 @@
-using Godot;
 using System;
+
+using Godot;
 
 public class AuthFlowController : Node
 {
--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -2,58 +2,58 @@ using Godot;
 
 public class Main : Node2D
 {
-	[Export] public string BackendBaseUrl = "http://127.0.0.1:8000";
+    [Export] public string BackendBaseUrl = "http://127.0.0.1:8000";
 
-	private PrototypeGameplaySession _gameplaySession;
-	private AuthFlowController _authFlowController;
-	private SidebarMenu _sidebarMenu;
+    private PrototypeGameplaySession _gameplaySession;
+    private AuthFlowController _authFlowController;
+    private SidebarMenu _sidebarMenu;
 
-	public override void _Ready()
-	{
-		SetupGameplaySession();
-		SetupSidebarMenu();
-		SetupAuthFlowController();
-	}
+    public override void _Ready()
+    {
+        SetupGameplaySession();
+        SetupSidebarMenu();
+        SetupAuthFlowController();
+    }
 
-	private void BuildPlayablePrototype()
-	{
-		_gameplaySession?.StartSession();
-		_sidebarMenu?.SetMenuVisible(true);
-	}
+    private void BuildPlayablePrototype()
+    {
+        _gameplaySession?.StartSession();
+        _sidebarMenu?.SetMenuVisible(true);
+    }
 
-	private void SetupGameplaySession()
-	{
-		_gameplaySession = new PrototypeGameplaySession();
-		AddChild(_gameplaySession);
-	}
+    private void SetupGameplaySession()
+    {
+        _gameplaySession = new PrototypeGameplaySession();
+        AddChild(_gameplaySession);
+    }
 
-	private void SetupAuthFlowController()
-	{
-		_authFlowController = new AuthFlowController
-		{
-			BackendBaseUrl = BackendBaseUrl
-		};
-		AddChild(_authFlowController);
-		_authFlowController.Authenticated += BuildPlayablePrototype;
-		_authFlowController.LoggedOut += HandleLoggedOut;
-	}
+    private void SetupAuthFlowController()
+    {
+        _authFlowController = new AuthFlowController
+        {
+            BackendBaseUrl = BackendBaseUrl
+        };
+        AddChild(_authFlowController);
+        _authFlowController.Authenticated += BuildPlayablePrototype;
+        _authFlowController.LoggedOut += HandleLoggedOut;
+    }
 
-	private void SetupSidebarMenu()
-	{
-		_sidebarMenu = new SidebarMenu();
-		AddChild(_sidebarMenu);
-		_sidebarMenu.SetMenuVisible(false);
-		_sidebarMenu.AddMenuItem("logout", "Logout", OnLogoutPressed);
-	}
+    private void SetupSidebarMenu()
+    {
+        _sidebarMenu = new SidebarMenu();
+        AddChild(_sidebarMenu);
+        _sidebarMenu.SetMenuVisible(false);
+        _sidebarMenu.AddMenuItem("logout", "Logout", OnLogoutPressed);
+    }
 
-	private void OnLogoutPressed()
-	{
-		_authFlowController?.RequestLogout();
-	}
+    private void OnLogoutPressed()
+    {
+        _authFlowController?.RequestLogout();
+    }
 
-	private void HandleLoggedOut()
-	{
-		_gameplaySession?.StopSession();
-		_sidebarMenu?.SetMenuVisible(false);
-	}
+    private void HandleLoggedOut()
+    {
+        _gameplaySession?.StopSession();
+        _sidebarMenu?.SetMenuVisible(false);
+    }
 }
